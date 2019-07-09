093488ce31404113325f8a9341d8e27c69e20104
GabrielaKlabunde <50932960+GabrielaKlabunde@users.noreply.github.com>
2019-07-09 12:29:28 -0300

    Uma forma de contador While

---
+++ Base contador.java
@@ -0,0 +1,16 @@
+
+package javaapplication1;
+
+public class JavaApplication1 {
+
+    public static void main(String[] args) {
+    //Escrerver números de 1 a 10
+    int contador = 1;
+    
+    while (contador <=10 ) {
+        System.out.println("Contador: "+contador);
+        contador++;
+    
+    }
+    
+}
